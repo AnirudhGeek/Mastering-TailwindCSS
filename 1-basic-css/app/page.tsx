750c883db1f9eaf5e85cf46b608644e4e25ea776
Anirudh Raturi <annigeek@gmail.com>
2026-03-28 23:53:47 +0530

    badge component

--- 1-basic-css/app/page.tsx
+++ 1-basic-css/app/page.tsx
@@ -1,18 +1,16 @@
 import Image from "next/image";
 
-
-
 import { Container } from "@/component/Container";
 import Navbar from "@/component/Navbar";
-
-
-
-
+import Hero from "@/component/hero";
 
 export default function Home() {
   return (
     <div className="layout">
-      <Container><Navbar/></Container>
+      <Container>
+        <Navbar />
+        <Hero/>
+      </Container>
     </div>
   );
 }
---
+++ 1-basic-css/component/hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero-root">
+      <div className="badge">
+        <span>What are Form-1099 filing requirements?</span>
+        <svg width="16" height="16" fill="none">
+          <path
+            stroke="#1E1F25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeOpacity=".5"
+            strokeWidth="1.25"
+            d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
